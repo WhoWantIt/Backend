0709d102069584aecc6b499518cdcd4678b12e3c
gaeul3303 <rkdmfghd@naver.com>
2025-01-14 20:17:59 +0900

    initial project

---
+++ src/main/java/gdg/whowantit/WhoWantItApplication.java
@@ -0,0 +1,13 @@
+package gdg.whowantit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoWantItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoWantItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdg/whowantit/WhoWantItApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.whowantit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoWantItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
