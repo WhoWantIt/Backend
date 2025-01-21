a3d6f553dd6f0ddee382c2fffdbf1c4bcfd9d6a6
gaeul3303 <rkdmfghd@naver.com>
2025-01-21 18:35:50 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/gdg/whowantit/apiPayload/code/BaseCode.java
@@ -0,0 +1,7 @@
+package gdg.whowantit.apiPayload.code;
+
+public interface BaseCode {
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/gdg/whowantit/apiPayload/exception/GeneralException.java
@@ -0,0 +1,20 @@
+package gdg.whowantit.apiPayload.exception;
+
+import gdg.whowantit.apiPayload.code.BaseErrorCode;
+import gdg.whowantit.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException{
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
