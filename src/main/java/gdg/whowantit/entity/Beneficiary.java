3718bd6046f646b3db4e16bcd292881bcb05ffc4
younghyun753 <younghyun75398@gmail.com>
2025-01-25 01:46:17 +0900

    feat: 매핑 관계 수정

--- src/main/java/gdg/whowantit/entity/Beneficiary.java
+++ src/main/java/gdg/whowantit/entity/Beneficiary.java
@@ -28,13 +28,8 @@ public class Beneficiary {
     @OneToMany(mappedBy = "beneficiary", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Funding> fundings = new ArrayList<>(); //beneficiary와 Funding 간의 1:N 관계
 
+
     @OneToMany(mappedBy = "beneficiary", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Volunteer> volunteers = new ArrayList<>(); //beneficiary와 Volunteer 간의 1:N 관계
-
-    @OneToMany(mappedBy = "beneficiary", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<FundingRelation> fundingRelations = new ArrayList<>(); //beneficiary와 fundingRelations 간의 1:N 관계
-
-    @OneToMany(mappedBy = "beneficiary", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<VolunteerRelation> volunteerRelations = new ArrayList<>(); //beneficiary와 volunteerRelations 간의 1:N 관계
 
 }
