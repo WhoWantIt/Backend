3dc356f7ccf2169e405fd2921ac73233294e43d3
younghyun753 <younghyun75398@gmail.com>
2025-01-27 01:02:15 +0900

    feat: 회원가입, 로그인 response, request dto 구현 (#16)

---
+++ src/main/java/gdg/whowantit/dto/request/SignUpRequestDto.java
@@ -0,0 +1,14 @@
+package gdg.whowantit.dto.request;
+import gdg.whowantit.entity.Role;
+import lombok.Getter;
+
+@Getter
+public class SignUpRequestDto {
+    private String name;
+    private Role role;
+    private String nickname;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String address;
+}
