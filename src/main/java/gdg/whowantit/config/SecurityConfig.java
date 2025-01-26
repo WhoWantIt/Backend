58bafd1bf81e218c65cd601e5091b88e892ae64e
gaeul3303 <rkdmfghd@naver.com>
2025-01-26 16:52:21 +0900

    bug: spring, swagger 의존성 문제 해결

--- src/main/java/gdg/whowantit/config/SecurityConfig.java
+++ src/main/java/gdg/whowantit/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,22 +17,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                //"/health" 추가 해야 할 수도
-                .csrf(csrf->csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/**", "/swagger-ui/**", "/v3/api-docs/**", "/members/signup", "/css/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home", true)
-                        .permitAll()
-                )
-                .logout((logout) -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll()
                 );
 
         return http.build();
--- src/main/java/gdg/whowantit/config/SwaggerConfig.java
+++ src/main/java/gdg/whowantit/config/SwaggerConfig.java
@@ -34,4 +34,5 @@ public class SwaggerConfig {
                 .info(info)
                 .components(authComponent());
     }
+
 }
