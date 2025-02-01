e62c73fb70d1a4de928da5848dc73679fd291852
younghyun753 <younghyun75398@gmail.com>
2025-02-01 17:34:07 +0900

    feat: 스웨거 관련 에러 처리, 토큰 없거나 잘못된 경우 에러처리 (#16)

--- src/main/java/gdg/whowantit/security/JwtAuthenticationFilter.java
+++ src/main/java/gdg/whowantit/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package gdg.whowantit.security;
 
+import gdg.whowantit.apiPayload.code.status.ErrorStatus;
+import gdg.whowantit.apiPayload.exception.handler.TempHandler;
 import gdg.whowantit.util.JwtUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
@@ -24,6 +26,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String token = extractToken(request);  // 요청에서 JWT 추출
+        String requestURI = request.getRequestURI();
+
+        // ✅ Swagger 관련 요청은 필터를 그냥 통과시키기
+        if (requestURI.startsWith("/swagger-ui") || requestURI.startsWith("/v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+        if (token == null) {
+            throw new TempHandler(ErrorStatus.TOKEN_NOT_FOUND); // ✅ 예외 던지기
+        }
+
+        if (!JwtUtil.validateToken(token)) {  // ✅ JWT 검증 실패 시
+            throw new TempHandler(ErrorStatus.TOKEN_EXPIRED); // ✅ 예외 던지기
+        }
+
+
 
         if (token != null && JwtUtil.validateToken(token)) {  // 정적 메서드 사용
             String email = JwtUtil.getEmailFromToken(token); // 이메일 추출
