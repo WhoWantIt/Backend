cea6d6a8c2003fdb54bdfeaeeb7c5475c017c88e
younghyun753 <younghyun75398@gmail.com>
2025-02-02 18:16:36 +0900

    feat: 필드 값 구체적으로 수정  ( #16 )

--- src/main/java/gdg/whowantit/entity/Field.java
+++ src/main/java/gdg/whowantit/entity/Field.java
@@ -1,5 +1,23 @@
 package gdg.whowantit.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum Field {
-    EDUCATION, HEALTH, ENVIRONMENT
+    LIVING_SUPPORT("생활 편의 지원"),
+    HOUSING_ENVIRONMENT("주거환경"),
+    COUNSELING("상담"),
+    EDUCATION("교육"),
+    HEALTHCARE("보건의료"),
+    CULTURAL_EVENTS("문화행사"),
+    ENVIRONMENTAL_PROTECTION("환경보호"),
+    DISASTER_RELIEF("재해 재난"),
+    PUBLIC_INTEREST_RIGHTS("공익 인권"),
+    MENTORING("멘토링");
+
+    private final String description; // 한글 설명
+
+    Field(String description) {
+        this.description = description;
+    }
 }
