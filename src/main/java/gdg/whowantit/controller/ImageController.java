92440520febcf73fe8fab8495d57800ac9e86ff9
gaeul3303 <rkdmfghd@naver.com>
2025-02-02 20:29:17 +0900

    feat:s3 구축 후, 이미지 업로드 API 구현

---
+++ src/main/java/gdg/whowantit/controller/ImageController.java
@@ -0,0 +1,26 @@
+package gdg.whowantit.controller;
+
+import gdg.whowantit.apiPayload.ApiResponse;
+import gdg.whowantit.service.ImageService.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class ImageController {
+    private final ImageService imageService;
+    @PostMapping(value="/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary="이미지 업로드 API",
+            description = "이미지 업로드 API")
+    public ApiResponse<String> uploadImage(@RequestParam("imageFile") MultipartFile image){
+        String imageUrl = imageService.uploadImage("image", image);
+        return ApiResponse.onSuccess(imageUrl);
+    }
+}
---
+++ src/main/java/gdg/whowantit/service/ImageService/ImageServiceImpl.java
@@ -0,0 +1,30 @@
+package gdg.whowantit.service.ImageService;
+
+import gdg.whowantit.apiPayload.code.status.ErrorStatus;
+import gdg.whowantit.apiPayload.exception.handler.TempHandler;
+import gdg.whowantit.aws.s3.AmazonS3Manager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService{
+    private final AmazonS3Manager s3Manager;
+
+    @Override
+    @Transactional
+    public String uploadImage (String directory, MultipartFile image){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
+            throw new TempHandler(ErrorStatus.TOKEN_EXPIRED);
+        }
+
+        return s3Manager.uploadFile(directory, image);
+
+    }
+}
