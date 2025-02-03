d5b88693895c9d406198bf6787170b77cac846e1
younghyun753 <younghyun75398@gmail.com>
2025-02-03 19:21:46 +0900

    feat: volunteerRelationRepo 구현 (#22)

---
+++ src/main/java/gdg/whowantit/repository/VolunteerRelationRepository.java
@@ -0,0 +1,14 @@
+package gdg.whowantit.repository;
+
+import gdg.whowantit.entity.Sponsor;
+import gdg.whowantit.entity.Volunteer;
+import gdg.whowantit.entity.VolunteerRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VolunteerRelationRepository extends JpaRepository<VolunteerRelation, Long> {
+    Optional<VolunteerRelation> findVolunteerRelationByVolunteer(Volunteer volunteer);
+    boolean existsVolunteerRelationBySponsor(Sponsor sponsor);
+    void deleteVolunteerRelationByVolunteerRelationId(Long volunteerRelationId);
+}
