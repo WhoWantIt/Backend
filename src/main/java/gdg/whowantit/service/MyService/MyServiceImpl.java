db4455e8a1fcf71fb74d0dcd04697fe3ad53b27c
gaeul3303 <rkdmfghd@naver.com>
2025-02-03 21:34:55 +0900

    bug: 충돌 해결

--- src/main/java/gdg/whowantit/service/MyService/MyServiceImpl.java
+++ src/main/java/gdg/whowantit/service/MyService/MyServiceImpl.java
@@ -10,21 +10,19 @@ import gdg.whowantit.entity.User;
 import gdg.whowantit.repository.UserRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-=======
->>>>>>> 1289d00 (feat: 사용자 개인정보 API 구현)
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MyServiceImpl implements MyService{
-    //private final JwtManager jwtManager;
+
     private final UserRepository userRepository;
 
-<<<<<<< HEAD
 
     @Override
     @Transactional
@@ -38,22 +36,13 @@ public class MyServiceImpl implements MyService{
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new TempHandler(ErrorStatus.USER_NOT_FOUND));
 
-=======
-    //userId를 token으로 바꿀 예정
-    @Override
-    @Transactional
-    public MyResponseDto.MyResponse getProfile(Long userId){
-        //Long userId=jwtManager.validateJwt(accessToken);
-        User user=userRepository.findById(userId)
-                .orElseThrow(()->new TempHandler(ErrorStatus.LOGIN_ERROR_ID));
->>>>>>> 1289d00 (feat: 사용자 개인정보 API 구현)
+
 
         return MyConverter.toMyResponse(user);
     }
 
     @Override
     @Transactional
-<<<<<<< HEAD
     public MyResponseDto.MyResponse updateProfile(MyRequestDto.MyRequest request){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -64,12 +53,6 @@ public class MyServiceImpl implements MyService{
         String email = authentication.getName(); // 현재 로그인된 사용자의 이메일
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new TempHandler(ErrorStatus.USER_NOT_FOUND));
-=======
-    public MyResponseDto.MyResponse updateProfile(Long userId, MyRequestDto.MyRequest request){
-        //Long userId=jwtManager.validateJwt(accessToken);
-        User user=userRepository.findById(userId)
-                .orElseThrow(()->new TempHandler(ErrorStatus.LOGIN_ERROR_ID));
->>>>>>> 1289d00 (feat: 사용자 개인정보 API 구현)
 
         user.setNickname(request.getNickname());
         user.setEmail(request.getEmail());
