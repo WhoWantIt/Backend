efb94a039ab92f7152a48c5da374d60e1c82b094
younghyun753 <younghyun75398@gmail.com>
2025-02-03 15:46:16 +0900

    feat: volunteer 생성 관련 응답 dto 수정 ( #22 )

--- src/main/java/gdg/whowantit/dto/request/VolunteerRequestDto.java
+++ src/main/java/gdg/whowantit/dto/request/VolunteerRequestDto.java
@@ -1,11 +1,15 @@
 package gdg.whowantit.dto.request;
 
 import gdg.whowantit.entity.Field;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VolunteerRequestDto {
     private String nickname;
     private String title;
@@ -13,6 +17,6 @@ public class VolunteerRequestDto {
     private String content;
     private String attachedImage;
     private String startTime;
-    private String deadLine;
+    private String deadline;
     private Long maxCapacity;
 }
--- src/main/java/gdg/whowantit/dto/response/VolunteerResponseDto.java
+++ src/main/java/gdg/whowantit/dto/response/VolunteerResponseDto.java
@@ -2,21 +2,27 @@ package gdg.whowantit.dto.response;
 
 import gdg.whowantit.entity.ApprovalStatus;
 import gdg.whowantit.entity.Field;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VolunteerResponseDto {
-    private String volunteerId;
-    private String beneficiaryId;
+    private Long volunteerId;
+    private Long beneficiaryId;
     private String nickname; // entity에는 포함되어 있지 않지만 사용자에게 응답 줄 때는 주는게 좋을 것 같아서
     private String title;
     private Field field;
     private String content;
     private String attachedImage;
-    private String startTime;
-    private String deadLine;
+    private LocalDateTime startTime;
+    private LocalDateTime deadline;
     private Long maxCapacity;
     private Long currentCapacity;
     private ApprovalStatus approvalStatus;
