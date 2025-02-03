dffba98529ef82d8dcafb89c87ab46ebab5fc07a
younghyun753 <younghyun75398@gmail.com>
2025-02-04 01:36:12 +0900

    feat: aws 관련 키 연결 안되는 에러 해결 및 jwt 설정 application.yml에 추가

--- src/main/java/gdg/whowantit/config/AmazonConfig.java
+++ src/main/java/gdg/whowantit/config/AmazonConfig.java
@@ -11,20 +11,18 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Getter
-
 public class AmazonConfig {
-    private AWSCredentials awsCredentials;
-    private final String accessKey;
-    private final String secretKey;
 
-    public AmazonConfig() {
-        this.accessKey = System.getenv("cloud.aws.credentials.accessKey");
-        this.secretKey = System.getenv("cloud.aws.credentials.secretKey");
-    }
+    private AWSCredentials awsCredentials;
+
+    @Value("${cloud.aws.credentials.accessKey}")  // application.yml에서 값 주입
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -36,11 +34,17 @@ public class AmazonConfig {
     private String picturePath;
 
     @PostConstruct
-    public void init() {this.awsCredentials=new BasicAWSCredentials(accessKey, secretKey);}
+    public void init() {
+        if (accessKey == null || secretKey == null || accessKey.isEmpty() || secretKey.isEmpty()) {
+            throw new IllegalArgumentException(" AWS Access Key 또는 Secret Key가 설정되지 않았습니다!");
+        }
+
+        //AWS 자격 증명 생성
+        this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    }
 
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
@@ -48,5 +52,7 @@ public class AmazonConfig {
     }
 
     @Bean
-    public AWSCredentialsProvider awsCredentialsProvider() {return new AWSStaticCredentialsProvider(awsCredentials);}
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
 }
