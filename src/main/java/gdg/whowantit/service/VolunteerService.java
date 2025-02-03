cc6f5a34206e1696ee7dd2ab077b0f7e6c2d2676
younghyun753 <younghyun75398@gmail.com>
2025-02-03 19:22:20 +0900

    feat: volunteerService 구현, 후원자의 volunteer 신청 부분 (#22)

--- src/main/java/gdg/whowantit/service/VolunteerService.java
+++ src/main/java/gdg/whowantit/service/VolunteerService.java
@@ -3,14 +3,19 @@ package gdg.whowantit.service;
 import gdg.whowantit.apiPayload.code.status.ErrorStatus;
 import gdg.whowantit.apiPayload.exception.handler.TempHandler;
 import gdg.whowantit.converter.VolunteerConverter;
+import gdg.whowantit.converter.VolunteerRelationConverter;
 import gdg.whowantit.dto.request.VolunteerRequestDto;
+import gdg.whowantit.dto.response.VolunteerRelationResponseDto;
 import gdg.whowantit.dto.response.VolunteerResponseDto;
 import gdg.whowantit.entity.ApprovalStatus;
 import gdg.whowantit.entity.User;
 import gdg.whowantit.entity.Volunteer;
+import gdg.whowantit.entity.VolunteerRelation;
 import gdg.whowantit.repository.BeneficiaryRepository;
 import gdg.whowantit.repository.UserRepository;
+import gdg.whowantit.repository.VolunteerRelationRepository;
 import gdg.whowantit.repository.VolunteerRepository;
+import gdg.whowantit.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,13 +29,10 @@ public class VolunteerService {
     private final VolunteerRepository volunteerRepository;
     private final UserRepository userRepository;
     private final BeneficiaryRepository beneficiaryRepository;
+    private final VolunteerRelationRepository volunteerRelationRepository;
     public VolunteerResponseDto postVolunteer(VolunteerRequestDto volunteerRequestDto) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
-            throw new TempHandler(ErrorStatus.TOKEN_EXPIRED);
-        }
 
-        String email = authentication.getName(); // 현재 로그인된 사용자의 이메일
+        String email = SecurityUtil.getCurrentUserEmail(); // 현재 로그인된 사용자의 이메일
         User user = userRepository.findByEmail(email).
                 orElseThrow(() -> new TempHandler(ErrorStatus.USER_NOT_FOUND));
 
@@ -48,5 +50,31 @@ public class VolunteerService {
         volunteerResponseDto.setVolunteerId(volunteer.getVolunteerId());
         volunteerResponseDto.setBeneficiaryId(user.getId());
         return volunteerResponseDto;
+    }
+
+    public VolunteerRelationResponseDto applyVolunteer(Long volunteerId) {
+        String email = SecurityUtil.getCurrentUserEmail();
+
+        User user = userRepository.findByEmail(email).
+                orElseThrow(() -> new TempHandler(ErrorStatus.USER_NOT_FOUND));
+
+
+        Volunteer volunteer = volunteerRepository.
+                findVolunteerByVolunteerId(volunteerId).
+                orElseThrow(() -> new TempHandler(ErrorStatus.VOLUNTEER_NOT_FOUND));
+
+        if (volunteerRelationRepository.existsVolunteerRelationBySponsor(user.getSponsor()))
+        {
+            throw new TempHandler(ErrorStatus.VOLUNTEER_ALREADY_APPLIED);
+        }
+
+        // VolunteerRelation 객체 생성해서 정보 입력
+        VolunteerRelation volunteerRelation = new VolunteerRelation();
+        volunteerRelation.setVolunteer(volunteer);
+        volunteerRelation.setBeneficiary(volunteer.getBeneficiary());
+        volunteerRelation.setSponsor(user.getSponsor());
+        volunteerRelationRepository.save(volunteerRelation);
+        return VolunteerRelationConverter.
+                convertVolunteerRelationToVolunteerResponseDto(volunteerRelation);
     }
 }
