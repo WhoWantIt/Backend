6733d77fbdd9e1ee3ab8d557e3be32c1356cfead
gaeul3303 <rkdmfghd@naver.com>
2025-02-03 23:27:11 +0900

    docs: AmazonConfig 파일 propertySource 추가

--- src/main/java/gdg/whowantit/config/AmazonConfig.java
+++ src/main/java/gdg/whowantit/config/AmazonConfig.java
@@ -11,9 +11,11 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Getter
+@PropertySource("classpath:application.yml")
 public class AmazonConfig {
     private AWSCredentials awsCredentials;
 
