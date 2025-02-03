8c32c6851df489db48b07afbc03d342b21fce3ce
younghyun753 <younghyun75398@gmail.com>
2025-02-03 23:20:33 +0900

    feat: volunteerRepo에 페이지네이션 추가 (#22)

--- src/main/java/gdg/whowantit/repository/VolunteerRepository.java
+++ src/main/java/gdg/whowantit/repository/VolunteerRepository.java
@@ -1,10 +1,19 @@
 package gdg.whowantit.repository;
 
+import gdg.whowantit.entity.ApprovalStatus;
 import gdg.whowantit.entity.Volunteer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
     Optional<Volunteer> findVolunteerByVolunteerId(Long volunteerId);
+    List<Volunteer> findByApprovalStatus(ApprovalStatus approvalStatus);
+    Page<Volunteer> findByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
+
 }
