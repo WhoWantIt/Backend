062f1dcb5461175507eda811ca056acf6de2fd79
gaeul3303 <rkdmfghd@naver.com>
2025-02-04 02:20:01 +0900

    docs: 환경변수명 재설정

--- src/main/java/gdg/whowantit/config/AmazonConfig.java
+++ src/main/java/gdg/whowantit/config/AmazonConfig.java
@@ -20,10 +20,10 @@ public class AmazonConfig {
     private AWSCredentials awsCredentials;
 
 
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${AWS_ACCESS_KEY_ID}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
 
     @Value("${cloud.aws.s3.bucket}")
