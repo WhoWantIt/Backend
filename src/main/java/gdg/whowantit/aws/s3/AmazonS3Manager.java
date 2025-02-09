c13b9f0a8ea0eaf95fdfb2b53df25e9dbdf1a614
younghyun753 <younghyun75398@gmail.com>
2025-02-10 01:58:04 +0900

    feat: 다중 업로드 구현 (#59)

--- src/main/java/gdg/whowantit/aws/s3/AmazonS3Manager.java
+++ src/main/java/gdg/whowantit/aws/s3/AmazonS3Manager.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -39,6 +41,15 @@ public class AmazonS3Manager {
         }
 
         return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    // ✅ 여러 개의 이미지 업로드 처리
+    public List<String> uploadMultipleFiles(String dirName, List<MultipartFile> files) {
+
+
+        return files.stream()
+                .map(file -> uploadFile(dirName, file))
+                .collect(Collectors.toList());
     }
 
     // S3의 이미지 삭제
