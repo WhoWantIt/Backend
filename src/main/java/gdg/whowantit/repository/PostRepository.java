b0e81d986ab44fa763bb0c96f0bdeffec1f2db8a
younghyun753 <younghyun75398@gmail.com>
2025-02-10 22:09:24 +0900

    feat: 년도, 월별 post 조회 repo 구현 (#61)

--- src/main/java/gdg/whowantit/repository/PostRepository.java
+++ src/main/java/gdg/whowantit/repository/PostRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByBeneficiary_beneficiaryId(Long beneficiaryId);
     Page<Post> findByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
     Page<Post> findByBeneficiary(Beneficiary beneficiary, Pageable pageable);
+    Page<Post> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
 }
+
+
