4a37306a015efaad9722e1ea7d9dd54570a64e56
younghyun753 <younghyun75398@gmail.com>
2025-02-10 21:46:31 +0900

    feat: 복지시설에 따른 post page 조회 추가 (#61)

--- src/main/java/gdg/whowantit/repository/PostRepository.java
+++ src/main/java/gdg/whowantit/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package gdg.whowantit.repository;
 
 import gdg.whowantit.entity.ApprovalStatus;
+import gdg.whowantit.entity.Beneficiary;
 import gdg.whowantit.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByBeneficiary_beneficiaryId(Long beneficiaryId);
     Page<Post> findByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
+    Page<Post> findByBeneficiary(Beneficiary beneficiary, Pageable pageable);
 }
