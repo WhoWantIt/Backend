fc8dfddb9d6157a609933c79247e72934cb373b8
younghyun753 <younghyun75398@gmail.com>
2025-02-10 21:37:52 +0900

    feat: 게시물 repo에 승인상태에 따른 조회 추가 (#61)

--- src/main/java/gdg/whowantit/repository/PostRepository.java
+++ src/main/java/gdg/whowantit/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package gdg.whowantit.repository;
 
+import gdg.whowantit.entity.ApprovalStatus;
 import gdg.whowantit.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByBeneficiary_beneficiaryId(Long beneficiaryId);
+    Page<Post> findByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
 }
