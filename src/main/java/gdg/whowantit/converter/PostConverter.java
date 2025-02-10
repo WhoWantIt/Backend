df9a4f1c3be70656dba2b3ff1fdfeb36b771a37f
younghyun753 <younghyun75398@gmail.com>
2025-02-10 19:29:31 +0900

    feat: postConverter 구현 (#61)

--- src/main/java/gdg/whowantit/converter/PostConverter.java
+++ src/main/java/gdg/whowantit/converter/PostConverter.java
@@ -1,7 +1,11 @@
 package gdg.whowantit.converter;
 
+import gdg.whowantit.dto.PostDto.PostRequestDto;
+import gdg.whowantit.dto.PostDto.PostResponseDto;
 import gdg.whowantit.dto.beneficiaryDto.BeneficiaryResponseDto;
 import gdg.whowantit.entity.Post;
+import gdg.whowantit.util.StringListUtil;
+import org.springframework.beans.BeanUtils;
 
 public class PostConverter {
     public static BeneficiaryResponseDto.postResponse toPostResponse (Post post){
@@ -13,5 +17,23 @@ public class PostConverter {
                 .beneficiaryName(post.getBeneficiary().getUser().getNickname())
                 .isVerified(post.isVerified())
                 .build();
+    }
+
+    public static PostResponseDto.BeneficiaryPostResponseDto toBeneficiaryPostResponseDto (Post post){
+
+        return PostResponseDto.BeneficiaryPostResponseDto.builder()
+                .postId(post.getPostId())
+                .beneficiaryId(post.getBeneficiary().getBeneficiaryId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .attachedExcelFile(post.getAttachedExcelFile())
+                .attachedImage(StringListUtil.stringToList(post.getAttachedImage()))
+                .build();
+    }
+
+    public static Post toPost (PostRequestDto.BeneficiaryPostRequestDto postRequestDto){
+        Post post = new Post();
+        BeanUtils.copyProperties(postRequestDto, post);
+        return post;
     }
 }
