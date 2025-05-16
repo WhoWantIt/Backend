e8307e1df334050271fdb7ed2e2e5d21712a1ca9
gaeul3303 <rkdmfghd@naver.com>
2025-05-16 17:55:53 +0900

    docs: nginx와 일치하는 단일 origin만 허용

--- src/main/java/gdg/whowantit/config/CorsConfig.java
+++ src/main/java/gdg/whowantit/config/CorsConfig.java
@@ -22,9 +22,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+/*
 
-        //리소스를 허용할 URL 지정
-        ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+ArrayList<String> allowedOriginPatterns = new ArrayList<>();
         allowedOriginPatterns.add("http://localhost:5173");
         allowedOriginPatterns.add("http://127.0.0.1:5000");
         allowedOriginPatterns.add("http://localhost:8080");
@@ -35,6 +35,10 @@ public class CorsConfig {
         allowedOriginPatterns.add("https://api.whowantit.me");
         allowedOriginPatterns.add("https://who-want-it.vercel.app");
         configuration.setAllowedOriginPatterns(allowedOriginPatterns);
+ */
+        configuration.setAllowedOrigins(List.of("https://www.whowantit.me"));
+        //리소스를 허용할 URL 지정
+
 
         //허용하는 HTTP METHOD 지정
         ArrayList<String> allowedHttpMethods = new ArrayList<>();
